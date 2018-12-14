1c901511be7b4cc65c68b6606794a8fa2d61979c
oldhu <me@oldhu.com>
2018-12-14 14:42:25 +0800

    added nfs related

--- ubuntu-install-docker.sh
+++ ubuntu-install-docker.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 
-sudo apt-get -y install \
+sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -15,9 +15,11 @@ sudo add-apt-repository \
 
 sudo apt-get update
 
-sudo apt-get -y install docker-ce
+sudo apt-get install -y docker-ce
 
 curl https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | sudo apt-key add - 
 echo "deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubectl
+
+sudo apt-get install -y nfs-common
