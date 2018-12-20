17d2293264d88cf45a6456edb19c75e399c67e37
oldhu <me@oldhu.com>
2018-12-20 20:35:00 +0800

    sed edit yaml file

--- qcloud/create-repo.sh
+++ qcloud/create-repo.sh
@@ -2,7 +2,9 @@ echo "Enter repo user name and press Enter:"
 user=$(sed '/^$/q')
 echo "Enter repo password and press Enter:"
 pass=$(sed '/^$/q')
-echo "Enter repo namespace/name and press Enter:"
+echo "Enter repo namespace and press Enter:"
+namespace=$(sed '/^$/q')
+echo "Enter repo name and press Enter:"
 repo=$(sed '/^$/q')
 
 cat <<EOF > priv-repo.sh
@@ -14,6 +16,13 @@ imageid=`sudo docker images | grep node-app | awk '{print $3}'`
 
 cat <<EOF > push-repo.sh
 sudo docker login --username $user --password $pass ccr.ccs.tencentyun.com
-sudo docker tag $imageid ccr.ccs.tencentyun.com/$repo:latest
-sudo docker push ccr.ccs.tencentyun.com/$repo:latest
+sudo docker tag $imageid ccr.ccs.tencentyun.com/$namespace/$repo:latest
+sudo docker push ccr.ccs.tencentyun.com/$namespace/$repo:latest
 EOF
+
+sed -i "s/tiejunhu/$namespace/g" deployment.yaml
+sed -i "s/tiejunhu/$namespace/g" deployment-nfs.yaml
+sed -i "s/tiejunhu/$namespace/g" deployment-cis.yaml
+sed -i "s/node-app-1/$repo/g" deployment.yaml
+sed -i "s/node-app-1/$repo/g" deployment-nfs.yaml
+sed -i "s/node-app-1/$repo/g" deployment-cis.yaml
